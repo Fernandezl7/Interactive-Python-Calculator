5b1929d3441f0daf18b122bace49b5f6e2fdb344
fernandezl7 <fernandezlizbeth5@gmail.com>
2025-03-13 23:21:41 -0400

    added calculator init

---
+++ apps/calculator/__init__.py
@@ -0,0 +1,24 @@
+from decimal import Decimal
+from typing import Callable
+from apps.calculator.calculations import Calculations
+from apps.calculator.calculation import Calculation
+from apps.calculator.operations import add,subtract,multiply,divide
+
+class Calculator:
+    @staticmethod
+    def _perform_operation(a:Decimal,b:Decimal,operation: Callable[[Decimal,Decimal],Decimal])-> Decimal:
+        calculation= Calculation.create(a,b,operation)
+        Calculations.add_calculation(calculation)
+        return calculation.perform()
+    @staticmethod
+    def add(a:Decimal,b:Decimal) -> Decimal:
+        return Calculator._perform_operation(a,b,add)
+    @staticmethod
+    def subtract(a:Decimal,b:Decimal)-> Decimal:
+        return Calculator._perform_operation(a,b,subtract)
+    @staticmethod
+    def multiply(a:Decimal,b:Decimal)->Decimal:
+        return Calculator._perform_operation(a,b,multiply)
+    def divide(a:Decimal,b:Decimal)-> Decimal:
+        return Calculator._perform_operation(a,b,divide)
+    
